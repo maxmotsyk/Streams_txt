ca6363ab52eb42f0afadd71d8af7a4c658f37679
maxmotsyk <21mechanic21@gmail.com>
2021-10-06 14:10:56 +0300

    cleare

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 
 namespace ConsoleApp1
 {
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-           
+            
+           System.Console.WriteLine("sosi");
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("sosi");
+        }
+    }
+}
